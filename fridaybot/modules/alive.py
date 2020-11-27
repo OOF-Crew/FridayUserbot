af479ac2968477ca7934bac9412d3bc9ae1ada3b
OOF-Crew <72276710+OOF-Crew@users.noreply.github.com>
2020-11-27 12:38:05 +0100

    Update alive.py

--- fridaybot/modules/alive.py
+++ fridaybot/modules/alive.py
@@ -41,20 +41,20 @@ def get_readable_time(seconds: int) -> str:
 uptime = get_readable_time((time.time() - Lastupdate))
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = Config.ALIVE_IMAGE
-pm_caption = "➥ **FRIDAY IS:** `ONLINE`\n\n"
-pm_caption += "➥ **SYSTEMS STATS**\n"
-pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
+pm_caption = "➥ **DOGGYCHEEMS:** `ONLINE`\n\n"
+pm_caption += "➥ **STATUS**\n"
+pm_caption += "➥ **Telethon:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.7.4` \n"
 pm_caption += f"➥ **Uptime** : `{uptime}` \n"
-pm_caption += "➥ **Database Status:**  `Functional`\n"
-pm_caption += "➥ **Current Branch** : `master`\n"
-pm_caption += f"➥ **Version** : `{currentversion}`\n"
-pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
-pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [StarkGang@Github](GitHub.com/StarkGang)\n"
-pm_caption += "➥ **Check Stats By Doing** `.stat`. \n\n"
-pm_caption += "[🇮🇳 Deploy FridayUserbot 🇮🇳](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "➥ **Database:**  `Functional`\n"
+pm_caption += "➥ **Branch** : `master`\n"
+pm_caption += f"➥ **Versione** : `{currentversion}`\n"
+pm_caption += f"➥ **Padrone** : {DEFAULTUSER} \n"
+pm_caption += "➥ **Heroku** : `AWS - Online`\n\n"
+pm_caption += "➥ **Licenza** : [GNU General Public License v3.0](github.com/OOF-Crew/FridayUserbot/blob/master/LICENSE)\n"
+pm_caption += "➥ **This is a mod of** : @fridayOT\n"
+pm_caption += "➥ **Controlla le statistiche** `.stat`. \n\n"
+pm_caption += "[Deploy original userbot](https://telegra.ph/FRIDAY-06-15)"
 
 
 @friday.on(friday_on_cmd(pattern=r"alive"))
@@ -70,6 +70,6 @@ CMD_HELP.update(
     {
         "alive": "**ALive**\
 \n\n**Syntax : **`.alive`\
-\n**Usage :** Check if UserBot is Alive"
+\n**Usage :** Verifica che il bot sia online"
     }
 )
